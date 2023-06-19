d61ff9f04916203b99a01a8316989151f4d1e20e
ajju8108 <ajaymaurya199618@gmail.com>
2023-06-19 17:21:03 +0530

    till slider done

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Xsdmr2c/1.png",
+        title: "SUMMER SALE",
+        desc: "Don't Compromise on style! get flat 30% off for new arrivals",
+        bg: "f5fafd"
+
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/Xsdmr2c/2.png",
+        title: "AUTUMN COLLECTION",
+        desc: "Don't Compromise on style! get flat 30% off for new arrivals",
+        bg: "fcf1ed"
+
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/Xsdmr2c/3.png",
+        title: "SUMMER SALE",
+        desc: "Don't Compromise on style! get flat 30% off for new arrivals",
+        bg: "fbf0f4"
+
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+    },
+];
